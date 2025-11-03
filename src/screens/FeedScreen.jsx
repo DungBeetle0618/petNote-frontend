3d445a0fbc95b67e8d219a5cf6da3faddbc288b7
dbrjsdml <dbrjsdml@naver.com>
2025-11-03 14:53:29 +0900

    커뮤니티 화면 추가

---
+++ src/screens/FeedScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+const FeedScreen = () => {
+
+    return (
+        <ScrollView>
+            <View><Text>FeedScreen</Text></View>        
+        </ScrollView>
+        
+    );
+};
+
+
+export default FeedScreen;
