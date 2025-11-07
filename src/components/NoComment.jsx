17fcfee5b969005658d1a8e64661c2c47b7088cd
dbrjsdml <dbrjsdml@naver.com>
2025-11-07 17:58:47 +0900

    댓글없는화면 컴포넌트

---
+++ src/components/NoComment.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NoComment = () => {
+  return (
+        <View style={{height:'100%', alignItems:'center', justifyContent:'center',}}>
+            <View>
+                <Text style={{fontSize:17, fontWeight:700, textAlign:'center'}}>아직 댓글이</Text>
+                <Text style={{fontSize:17, fontWeight:700, textAlign:'center'}}>없습니다.</Text>
+                <Text style={{fontSize:13, fontWeight:500, color:'777', marginTop:5}}>대화를 시작하세요.</Text>
+            </View>
+        </View>
+  )
+}
+
+export default CoNoCommentmment;
