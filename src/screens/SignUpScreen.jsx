fab68202a68eefc96ae74adf7024d7b98037434a
swkim <swkim@wezlesoft.co.kr>
2025-11-10 14:38:28 +0900

    회원가입 스크린 작업

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useRef, useState} from "react";
+import { View, Text, TextInput, Button } from "react-native";
 
 const SignUpScreen = () => {
+    const [id, setId] = useState('');
 
-    return
-     <View>
-        <Button
-         title='로그인 화면으로'
-         onPess={()=>navigation.navigate('login')}/>
-    </View>
+    return (
+        <View>
+            <Text>아이디</Text>
+                <TextInput
+                    value={id}
+                    onChangeText={setId}
+                    
+                />
+
+            <Button
+            title='로그인 화면으로'
+            onPess={()=>navigation.navigate('login')}/>
+        </View>
+    )
 }
 
 export default SignUpScreen;
