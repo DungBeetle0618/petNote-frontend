bcccdf5fa10c8097facc7fdad3a6f18ec36b2b54
DungBeetle0618 <bugreport1@naver.com>
2025-11-21 11:13:15 +0900

    마이 페이지 커밋

--- src/screens/MypageScreen.jsx
+++ src/screens/MypageScreen.jsx
@@ -2,7 +2,7 @@
  * 마이페이지
  */
 import React from 'react';
-import { View, Text, ScrollView, Pressable } from 'react-native';
+import { View, Text, ScrollView, Pressable, StyleSheet } from 'react-native';
 
 import EBoldText from '../components/font/EBoldText';
 import gs from '../assets/styles/globalStyles';
@@ -13,22 +13,50 @@ const MypageScreen = () => {
   
   return (
     <ScrollView contentContainerStyle={gs.screen}>
+      <View style={style.header}>
+        <View style={style.profileInfo}>
+          <View>
+            <Text>사진</Text>
+          </View>
+          <View>
+            <Text>bugreport1</Text>
+            <Text>bugreport1@naver.com</Text>
+          </View>
+        </View>
+        <View>
+          
+        </View>
+      </View>
 
-        <EBoldText style={gs.title}>마이페이지입니다.</EBoldText>
 
+      <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Reminder')}}>
+        <Text>리마인더 페이지 가기</Text>
+      </Pressable>
+      <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Challenge')}}>
+        <Text>챌린지 페이지 가기</Text>
+      </Pressable>
+      <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Product')}}>
+        <Text>상품 페이지 가기</Text>
+      </Pressable>
 
-        <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Reminder')}}>
-          <Text>리마인더 페이지 가기</Text>
-        </Pressable>
-        <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Challenge')}}>
-          <Text>챌린지 페이지 가기</Text>
-        </Pressable>
-        <Pressable style={{marginBlock: 20}} onPress={()=>{navigation.navigate('Product')}}>
-          <Text>상품 페이지 가기</Text>
-        </Pressable>
-
-      </ScrollView>
+    </ScrollView>
   );
 };
+
+const style = StyleSheet.create({
+  header: {
+    backgroundColor: "#FF6600",
+    paddingTop: 40,
+    paddingHorizontal: 24,
+    paddingBottom:24,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24
+  },
+
+  profileInfo: {
+    flexDirection
+  }
+
+})
 
 export default MypageScreen;
